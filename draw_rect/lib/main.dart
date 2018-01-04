ae80fb019086da767535270d31b94da3ae3bd6df
kyorohiro <kyorohiro@gmail.com>
2018-01-05 04:37:23 +0900

    add draw rect example

---
+++ draw_rect/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart' as sky;
+import 'package:flutter/widgets.dart' as sky;
+import 'package:flutter/painting.dart' as sky;
+import 'package:flutter/rendering.dart' as sky;
+
+void main() {
+  sky.runApp(new DrawRectWidget());
+}
+
+class DrawRectWidget extends sky.SingleChildRenderObjectWidget {
+  sky.RenderObject createRenderObject(sky.BuildContext context){
+    return new DrawRectObject();
+  }
+}
+
+class DrawRectObject extends sky.RenderBox {
+  void paint(sky.PaintingContext context, sky.Offset offset) {
+    sky.Paint p = new sky.Paint();
+    p.color = new sky.Color.fromARGB(0xff, 0x55, 0x55, 0x55);
+    sky.Rect r = new sky.Rect.fromLTWH(50.0, 100.0, 150.0, 25.0);
+    context.canvas.drawRect(r, p);
+  }
+}
