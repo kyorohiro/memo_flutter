8239eff358ad6d7675f4d5f9fae5acde1cbe5e6f
kyorohiro <kyorohiro@gmail.com>
2018-01-05 02:59:39 +0900

    first commit

---
+++ hello/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart' as sky;
+
+void main() {
+  sky.runApp( new sky.MaterialApp(
+      home: new sky.Center (
+      child: new sky.Text("Hello World",
+      style: new sky.TextStyle(color:new sky.Color.fromARGB(0xff,0xff,0xff,0xff)))),
+  ));
+}
