50c5f7eb182c34f4b9f3ae35a60588b441e2f283
kyorohiro <kyorohiro@gmail.com>
2018-01-05 05:17:51 +0900

    add animeRect ex

---
+++ anime_rect/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart' as sky;
+import 'package:flutter/widgets.dart' as sky;
+import 'package:flutter/painting.dart' as sky;
+import 'package:flutter/rendering.dart' as sky;
+import 'dart:async';
+import 'dart:math';
+
+void main() {
+  sky.runApp(new DrawRectWidget()..anime());
+}
+
+class DrawRectWidget extends sky.SingleChildRenderObjectWidget {
+  DrawRectObject drawObj = new DrawRectObject();
+  sky.RenderObject createRenderObject(sky.BuildContext context){
+    return drawObj;
+  }
+  Future anime() async {
+    double angle = 0.0;
+    while (true) {
+      await new Future.delayed(new Duration(milliseconds: 20));
+      drawObj.x = 150 * cos(PI * angle / 180.0) + 100.0;
+      drawObj.y = 150 * sin(PI * angle / 180.0) + 100.0;
+      angle++;
+      drawObj.markNeedsPaint();
+    }
+  }
+}
+
+class DrawRectObject extends sky.RenderBox {
+  double x = 50.0;
+  double y = 50.0;
+  void paint(sky.PaintingContext context, sky.Offset offset) {
+    sky.Paint p = new sky.Paint();
+    p.color = new sky.Color.fromARGB(0xff, 0xff, 0xff, 0xff);
+    sky.Rect r = new sky.Rect.fromLTWH(x, y, 25.0, 25.0);
+    context.canvas.drawRect(r, p);
+  }
+}
