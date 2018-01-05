5a29bde584cb6984ca19b7051050bd31e329f90f
kyorohiro <kyorohiro@gmail.com>
2018-01-05 17:16:25 +0900

    ..

--- draw_image_assets/lib/main.dart
+++ draw_image_assets/lib/main.dart
@@ -1,33 +1,30 @@
 // following code is checked in 2016/03/16
 
 import 'dart:async';
-import 'dart:ui' as sky;
+import 'dart:ui' as skyui;
 
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
-//
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter/rendering.dart' as sky;
+import 'package:flutter/services.dart' as sky;
+import 'package:flutter/widgets.dart' as sky;
+import 'package:flutter/material.dart' as sky;
+import 'package:flutter/painting.dart' as sky;
+
 void main() {
-  runApp(new DemoWidget());
+  sky.runApp(new DemoWidget());
 }
 
-class DemoWidget extends SingleChildRenderObjectWidget {
+class DemoWidget extends sky.SingleChildRenderObjectWidget {
   @override
-  RenderObject createRenderObject(BuildContext context) {
+  sky.RenderObject createRenderObject(sky.BuildContext context) {
     return new DemoObject();
   }
 }
 
-class DemoObject extends RenderConstrainedBox {
+class DemoObject extends sky.RenderConstrainedBox {
   double x = 50.0;
   double y = 50.0;
-  sky.Image image = null;
-  DemoObject() : super(additionalConstraints: const BoxConstraints.expand()) {
+  skyui.Image image = null;
+  DemoObject() : super(additionalConstraints: const sky.BoxConstraints.expand()) {
     ;
   }
   loadImage() async {
@@ -38,18 +35,18 @@ class DemoObject extends RenderConstrainedBox {
   }
 
   @override
-  bool hitTestSelf(Offset position) => true;
+  bool hitTestSelf(sky.Offset position) => true;
 
   @override
-  void handleEvent(PointerEvent event, BoxHitTestEntry entry) {}
+  void handleEvent(sky.PointerEvent event, sky.BoxHitTestEntry entry) {}
 
   @override
-  void paint(PaintingContext context, Offset offset) {
+  void paint(sky.PaintingContext context, sky.Offset offset) {
     loadImage();
-    Paint paint = new Paint()..color = new Color.fromARGB(0xff, 0xff, 0xff, 0xff);
-    Offset point = new Offset(x, y);
+    sky.Paint paint = new sky.Paint()..color = new sky.Color.fromARGB(0xff, 0xff, 0xff, 0xff);
+    sky.Offset point = new sky.Offset(x, y);
     if (image == null) {
-      Rect rect = new Rect.fromLTWH(x, y, 50.0, 50.0);
+      sky.Rect rect = new sky.Rect.fromLTWH(x, y, 50.0, 50.0);
       context.canvas.drawRect(rect, paint);
     } else {
       context.canvas.drawImage(image, point, paint);
@@ -59,16 +56,15 @@ class DemoObject extends RenderConstrainedBox {
 }
 
 class ImageLoader {
-  static AssetBundle getAssetBundle() => (rootBundle != null)
-      ? rootBundle
-      : new NetworkAssetBundle(new Uri.directory(Uri.base.origin));
-
-
-  static Future<sky.Image> load(String url) async {
-    ImageStream stream = new AssetImage(url, bundle: getAssetBundle()).resolve(ImageConfiguration.empty);
-    Completer<sky.Image> completer = new Completer<sky.Image>();
-    void listener(ImageInfo frame, bool synchronousCall) {
-      final sky.Image image = frame.image;
+  static sky.AssetBundle getAssetBundle() => (sky.rootBundle != null)
+      ? sky.rootBundle
+      : new sky.NetworkAssetBundle(new Uri.directory(Uri.base.origin));
+  
+  static Future<skyui.Image> load(String url) async {
+    sky.ImageStream stream = new sky.AssetImage(url, bundle: getAssetBundle()).resolve(sky.ImageConfiguration.empty);
+    Completer<skyui.Image> completer = new Completer<skyui.Image>();
+    void listener(sky.ImageInfo frame, bool synchronousCall) {
+      final skyui.Image image = frame.image;
       completer.complete(image);
       stream.removeListener(listener);
     }
